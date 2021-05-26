9ca84bf0e8c8b4685646b44d3e96f9a843f3b0d7
Luiz Felipe Gorla <lfsgorla@gmail.com>
2021-05-26 16:41:01 -0300

    Added More Code

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function showD() {
+  document.getElementById("code").style.display = "block"
+  document.getElementById("showD").style.display = "none"
+  document.getElementById("hideD").style.display = "inline-block"
+  document.getElementById("brToCode").style.display = "block"
+}
+function hideD() {
+  document.getElementById("code").style.display = "none"
+  document.getElementById("hideD").style.display = "none"
+  document.getElementById("showD").style.display = "inline-block"
+  document.getElementById("brToCode").style.display = "none"
+}
