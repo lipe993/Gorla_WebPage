d90d7b5d990422fa1654f4cf905e7893ff5c89be
Luiz Felipe Gorla <lfsgorla@gmail.com>
2022-06-17 16:42:43 -0300

    version 2.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Gorla Web Page
-## [![Version](https://img.shields.io/badge/version-2.0.1-blue)](https://lipe993.github.io/Gorla_WebPage/)
+## [![Version](https://img.shields.io/badge/version-2.1-blue)](https://lipe993.github.io/Gorla_WebPage/)
 ## [![Build status](https://ci.appveyor.com/api/projects/status/w030o09thupfi2cc?svg=true)](https://ci.appveyor.com/project/lipe993/gorla-webpage)
 [![My logo](./css/gorla.png)](https://lipe993.github.com/Gorla_WebPage)
 # Table of Contents
@@ -7,10 +7,8 @@
  - [Updates](#updates)
 
 ## Updates
-- Added "Going Down"
-- Added type writer effect
-- Tab indentation to "css/styles.css"
-- Added ".gitignore"
+- Added "Forever Chill"
+- Bug fixes
 
 ## To-Do list
 - [ ] Add a music visualizer
--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ const mus_button_icons = [
 	`<svg id="play-pause" xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 8.64L15.27 12 10 15.36V8.64M8 5v14l11-7L8 5z"/></svg>`,
 	`<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>`
 ]
+const canvas = document.getElementById("canvas1");
 var mus_info = {
 	album: 0,
 	track: 0,
+	lastTrack: 0,
 	format: 0,
 	path: 0
 };
@@ -27,7 +29,15 @@ const musNames = {
 		"Lo-Fi + Hip-Hop II",
 		"Hard Day"
 	],
-	goingdown: ["Going Down"],
+	foreverchill: [
+		"Time to go",
+		"chill out",
+		"in a calm place.",
+		"Chilling Sleep",
+		"Night of Joy",
+		"Going Down",
+		"forever chill."
+	]
 }
 
 function showHide(id) {
@@ -35,13 +45,14 @@ function showHide(id) {
 	if(findId.style.display == "none") findId.style.display = "block"; 
 	else findId.style.display = "none";
 }
-function typeWriter(message, speed, id, i) {
+function typeWriter(message, speed, id, i = 0) {
 	let findId = document.getElementById(id);
 	if(i < message.length) {
 		findId.innerHTML += message.charAt(i);
 		setTimeout(typeWriter, speed, message, speed, id, i + 1);
 	}
 }
+typeWriter("Gorla Music", 70, "headerText");
 audio1.addEventListener("playing", () => {
 	document.getElementById("info").className = "goto";
 	document.getElementById("play-pause").innerHTML = mus_button_icons[1];
@@ -69,15 +80,16 @@ document.getElementById("soundcloud").addEventListener("click", () => {
 document.getElementById("youtube").addEventListener("click", () => {
 	window.open("https://www.youtube.com/channel/UCds8sX73xHVC2XrJs9xsdaA");
 });
-function musWork(album, track, tracksFileFormat) {
+function musWork(album, track, lastTrack, tracksFileFormat = "wav") {
 	var currentTrack = track;
-    document.getElementById("musplayer").style.display = "block";
+	document.getElementById("musplayer").style.display = "block";
 	audio1.src = `audio/${album}/track-${currentTrack}.${tracksFileFormat}`;
 	mus_info.path = audio1.src;
 	audio1.load();
 	audio1.play();
 	mus_info.album = album;
 	mus_info.track = currentTrack;
+	mus_info.lastTrack = lastTrack;
 	mus_info.format = tracksFileFormat;
 	var albumPatch;
 	var albumToCall;
@@ -90,32 +102,41 @@ function musWork(album, track, tracksFileFormat) {
 			albumToCall = musNames.chillbeats;
 			albumPatch = "Chill Beats";
 			break;
-		case "goingdown":
-			albumToCall = musNames.goingdown;
-			albumPatch = "Going Down - Single";
+		case "foreverchill":
+			albumToCall = musNames.foreverchill;
+			albumPatch = "Forever Chill";
 			break;
 	}
-	document.getElementById("musName").innerHTML = "\0";
-	typeWriter(albumToCall[currentTrack - 1], 70, "musName", 0);
+	document.getElementById("musName").innerHTML = "";
+	typeWriter(albumToCall[currentTrack - 1], 70, "musName");
 	document.getElementById("musAlbum").innerHTML = albumPatch;
+	canvas.width = document.getElementById("musplayer").width;
+	canvas.height = document.getElementById("musplayer").height;
 }
 document.getElementById("chillbeats").addEventListener("click", () => {
-    musWork("chillbeats", 1, "wav");
+	musWork("chillbeats", 1, 6);
 });
 document.getElementById("chilllofi").addEventListener("click", () => {
-	musWork("chilllofi", 1, "mp3");
+	musWork("chilllofi", 1, 7, "mp3");
 });
-document.getElementById("goingdown").addEventListener("click", () => {
-	musWork("goingdown", 1, "ogg");
+document.getElementById("foreverchill").addEventListener("click", () => {
+	musWork("foreverchill", 1, 7);
 });
 document.getElementById("go-back").addEventListener("click", () => {
-	musWork(mus_info.album, mus_info.track - 1, mus_info.format);
+	if(mus_info.track - 1 == 0) {
+		musWork(mus_info.album, mus_info.lastTrack, mus_info.lastTrack, mus_info.format);
+	} else {
+		musWork(mus_info.album, mus_info.track - 1, mus_info.lastTrack, mus_info.format);
+	}
 });
 document.getElementById("go-on").addEventListener("click", () => {
-	musWork(mus_info.album, mus_info.track + 1, mus_info.format);
+	if(mus_info.track + 1 >= mus_info.lastTrack + 1) {
+		musWork(mus_info.album, 1, mus_info.lastTrack, mus_info.format);
+	} else {
+		musWork(mus_info.album, mus_info.track + 1, mus_info.lastTrack, mus_info.format);
+	}
 });
 document.getElementById("close").addEventListener("click", () => {
 	audio1.pause();
 	document.getElementById("musplayer").style.display = "none";
 });
-typeWriter("Gorla Music", 70, "headerText", 0);
